1836e93d3a46c5f011109262615c984beb30bde6
ITheBroncoI <136506399+ITheBroncoI@users.noreply.github.com>
2024-09-22 18:44:45 -0600

    Redesign Header (#20)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
+<<<<<<< HEAD
+=======
 import { WorkExperienceComponent } from "./work-experience/work-experience.component";
 import { EducationComponent } from "./education/education.component";
+>>>>>>> dc6893e498244182f0f77da4dad9e95e4d53edd7
 
 @Component({
   selector: 'app-root',
