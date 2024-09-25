f07c941e245db9ad8d2bc5c30376035ee55435e7
ITheBroncoI <136506399+ITheBroncoI@users.noreply.github.com>
2024-09-25 04:42:24 -0600

    Merge Pull Request #25 From ithebroncoi/language
    add language branch Closes #3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { WorkExperienceComponent } from "./work-experience/work-experience.compo
 import {EducationComponent} from "./education/education.component";
 import {SkillsComponent} from "./skills/skills.component";
 import {ArchivementsComponent} from "./archivements/archivements.component";
+import {LanguageComponent} from "./language/language.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, WorkExperienceComponent, EducationComponent, SkillsComponent, ArchivementsComponent,],
+  imports: [RouterOutlet, HeaderComponent, WorkExperienceComponent, EducationComponent, SkillsComponent, ArchivementsComponent, LanguageComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
